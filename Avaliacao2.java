a928ad4c8065843aea5625ca3d26bd6bb3f13e0b
Laryssa Adrielly  dos Santos Soares <80859127+adrielaryssa@users.noreply.github.com>
2021-05-16 19:30:20 -0300

    Add files via upload

---
+++ Avaliacao2.java
@@ -0,0 +1,17 @@
+
+package avaliacao2;
+
+import java.util.Scanner;
+
+public class Avaliacao2 {
+
+    public static void main(String[] args) { 
+        int c;
+        c=1000;
+        while(c>=100){
+            System.out.println(c);
+            c--;    
+        }
+    }    
+}
+    
